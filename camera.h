3fb6774e62be4ba4bf6f0941d94d94374836cf9c
Carefree <1173653942@qq.com>
2024-05-27 23:29:43 +0800

    A Scene with Metal Spheres

--- camera.h
+++ camera.h
@@ -4,6 +4,7 @@
 #include "rtweekend.h"
 
 #include "hittable.h"
+#include "material.h"
 
 class camera
 {
@@ -104,9 +105,11 @@ private:
 
         if (world.hit(r, interval(0.001, infinity), rec))
         {
-            vec3 direction = random_on_hemisphere(rec.normal);
-            // vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth - 1, world);
+            return color(0, 0, 0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
